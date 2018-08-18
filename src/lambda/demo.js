9531987b2771238e0eb1f625b5c06a1131ca0d3a
Martin <skydiver@users.noreply.github.com>
2018-08-17 22:50:00 -0300

    demo endpoint returns json and dummy data

--- src/lambda/demo.js
+++ src/lambda/demo.js
@@ -1,6 +1,16 @@
+const rn = require('random-number');
+const useragent = require('useragent');
+
 exports.handler = function(event, context, callback) {
   callback(null, {
     statusCode: 200,
-    body: 'Hello, World'
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      status: 'success',
+      random: rn({ min: 0, max: 10000000, integer: true }),
+      useragent: useragent.parse(event.headers['user-agent'])
+    })
   });
 };
